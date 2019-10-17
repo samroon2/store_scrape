4e8f3b8c136a9bf01678b29fb16940a00d0a77fe
samroon2 <sam.roon2454@gmail.com>
2019-10-16 23:13:21 -0400

    Tests.

--- store_scrape/store_data/store_codes.py
+++ store_scrape/store_data/store_codes.py
@@ -16,7 +16,7 @@ class CountryCodes:
 
     def load_alt_codes(cls, pickle_file):
         with open(pickle_file, 'rb') as f:
-            self.codes = pickle.load(f)
+            self.codes = pickle.load(f) 
 
     @property
     def countries(self):
--- tests/store_info_tests.py
+++ tests/store_info_tests.py
@@ -18,21 +18,33 @@ class BasicTests(unittest.TestCase):
 ###############
 
     def test_popular_apps_info(self):
+        '''Test for popular app method.
+        '''
         health = GetStoreInfo(url)
         health.get_popular_apps()
         assert len(health.popular_titles) != 0
 
     def test_get_genres(self):
+        '''Test for getting genres from the app store.
+        '''
         health = GetStoreInfo(url)
         health.get_genres()
-        print(health.genres)
         assert len(health.genres) != 0        
 
     def test_alpha_list(self):
+        '''Test for determining the alpha list in the app store.
+        '''
         health = GetStoreInfo(url)
         health.get_alpha_lists()
         assert len(health.alpha) != 0
 
+    def test_page_list(self):
+        '''Test for determining the alpha list in the app store.
+        '''
+        health = GetStoreInfo(url)
+        health.get_alpha_lists()
+        health.get_page_list(health.alpha[0])
+        assert len(health.pages) != 0
 
 if __name__ == "__main__":
     unittest.main()
