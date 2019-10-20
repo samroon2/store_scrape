e6dc8d94f1d9d03cfc33f3412f94d34916e918b8
samroon2 <sam.roon2454@gmail.com>
2019-10-20 19:05:33 -0400

    Tests.

--- tests/scrape_store_tests.py
+++ tests/scrape_store_tests.py
@@ -2,6 +2,7 @@ import base64
 import json
 import os
 import re
+import shutil
 import sys
 import unittest
 sys.path.append('../store_scrape/app_data')
@@ -21,9 +22,12 @@ class BasicTests(unittest.TestCase):
     def test_get_popular_apps(self):
         '''Test to get popular apps.
         '''
-        dats = ScrapetheStore(urlstart=url, country="Australia")
+        dats = ScrapetheStore(urlstart=url, country="United States")
         print(dats.urlstart)
         dats.get_top_apps(top=5)
+        downloads = [x for x in os.listdir('.') if '.py' not in x]
+        assert len(downloads) > 0
+        [shutil.rmtree(f"./{x}") for x in downloads]
 
 if __name__ == "__main__":
     unittest.main()
